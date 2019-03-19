bcb76f7c449056b3b74c97e227fde3bd1aa706bc
Samuel Batissou <samuelbatissou@gmail.com>
2019-03-19 11:45:44 +0100

    Some refacto

--- GoNNAgent.py
+++ GoNNAgent.py
@@ -6,207 +6,212 @@ import numpy as np
 import ops
 from random import randint, shuffle
 
+
 class GoNNAgent():
 
-	def __init__(self, board_size):
-		print("--- Initialization of Go Agent")
-		self.board_size = board_size
-		self.neural_network = GoNeuralNetwork(board_size)
-		self.g_old = np.full((1, board_size, board_size, 2), 0)
-		print("Initialized - Agent")
+    def __init__(self, board_size):
+        print("--- Initialization of Go Agent")
+        self.board_size = board_size
+        self.neural_network = GoNeuralNetwork(board_size)
+        self.g_old = np.full((1, board_size, board_size, 2), 0)
+        print("Initialized - Agent")
 
-	def get_move(self, goban, player_turn, legals):
-		#goban = self.goban_to_nn_state(goban)
-		"""if player_turn == 1:
-			goban = tuple(reversed(goban))
-		goban = np.array(goban)
-		g0 = ops.goban_to_nn_state(goban[0], self.board_size)
-		g1 = ops.goban_to_nn_state(goban[1], self.board_size)
-		goban = np.concatenate([g0, g1], axis=3)"""
-		goban, self.g_old = ops.goban_to_input_planes(goban, self.g_old, player_turn, self.board_size, dtype = int)
-		
-		player_feature_plane = np.full([self.board_size, self.board_size], player_turn)
-		player_feature_plane = ops.goban_to_nn_state(player_feature_plane, self.board_size)
-		planes = np.concatenate([goban, player_feature_plane], axis=3)
-		
-		p, v = self.neural_network.get_move(planes, player_turn, legals)
-		move = np.argmax(p)
-		return move
-		
-	def end_game(self, winner):
-		self.neural_network.save_in_replay_memory(winner)
-		
-	def supervised_training(self, dataset, k_fold = 0):
-		# Training parameters
-		epoch = 500000
-		report_frequency = 5
-		validation_frequency = 500
-		save_frequency = 1000
-		batch_size = 32
-		k = k_fold # k-fold cross validation
-		data_size = 3000
-		test_ratio = 1/30 # DeepMind paper		
-		random.seed(0)
+    def get_move(self, goban, player_turn, legals):
+        # goban = self.goban_to_nn_state(goban)
+        """if player_turn == 1:
+            goban = tuple(reversed(goban))
+        goban = np.array(goban)
+        g0 = ops.goban_to_nn_state(goban[0], self.board_size)
+        g1 = ops.goban_to_nn_state(goban[1], self.board_size)
+        goban = np.concatenate([g0, g1], axis=3)"""
+        goban, self.g_old = ops.goban_to_input_planes(goban, self.g_old, player_turn, self.board_size, dtype=int)
 
-		maxK = int(1/test_ratio)
-		k = min(k, maxK-1)
-		
-		# Load dataset
-		print("Data loading")
-		npzfile = np.load(dataset)
-		t_states = npzfile['states']
-		t_policies = npzfile['policies']
-		t_values = npzfile['values']
-		player_turn = npzfile['player_turn']
+        player_feature_plane = np.full([self.board_size, self.board_size], player_turn)
+        player_feature_plane = ops.goban_to_nn_state(player_feature_plane, self.board_size)
+        planes = np.concatenate([goban, player_feature_plane], axis=3)
 
-		# TODO - remove the next lines to work on the full dataset
-		# pre-shuffle
-		print("(pre-shuffle)")
-		temp = list(zip(t_states, t_policies, t_values))
-		shuffle(temp)
-		t_states, t_policies, t_values = zip(*temp)
-		t_states, t_policies, t_values = np.array(t_states), np.array(t_policies), np.array(t_values)
-		
-		t_states = t_states[:data_size]
-		t_policies = t_policies[:data_size]
-		t_values = t_values[:data_size]
-		player_turn = player_turn[:data_size]
+        p, v = self.neural_network.get_move(planes, player_turn, legals)
+        move = np.argmax(p)
+        return move
 
-		# Shape states to neural network input shape
-		print("Data shaping")
-		tt_states = []
-		tt_policies = []
-		for i in range(len(t_states)):
-			player_feature_plane = np.full((self.board_size, self.board_size), player_turn[i])
-			#t_state = ops.goban_to_nn_state(t_states[i], self.board_size)
-			t_state = t_states[i]
-			player_feature_plane = ops.goban_to_nn_state(player_feature_plane, self.board_size)
-			tt_states.append(np.concatenate([t_state, player_feature_plane], axis=3))
-			tt_policies.append(np.reshape(t_policies[i], (1, self.board_size ** 2 + 1)))
-		t_states = np.array(tt_states)
-		t_policies = np.array(tt_policies)
-		
-		input_planes = t_states.shape[-1]
-		
-		# Data augmentation 
-		print("Data augmentation")
-		states, policies, values = [], [], []
-		for i in range(len(t_states)):
-			state, policy, value = t_states[i], t_policies[i], t_values[i]
-			new_states, new_policies = ops.data_augmentation(state, policy, self.board_size)
-			for j in range(len(new_states)):
-				states.append(new_states[j])
-				policies.append(new_policies[j])
-				values.append(value)
+    def end_game(self, winner):
+        self.neural_network.save_in_replay_memory(winner)
 
-		# Shuffle
-		print("Data shuffling")
-		temp = list(zip(states, policies, values))
-		shuffle(temp)
-		states, policies, values = zip(*temp)
-		states, policies, values = np.array(states), np.array(policies), np.array(values)
+    def supervised_training(self, dataset, k_fold=0):
+        # Training parameters
+        epoch = 500000
+        report_frequency = 5
+        validation_frequency = 500
+        save_frequency = 1000
+        batch_size = 32
+        k = k_fold  # k-fold cross validation
+        data_size = 3000
+        test_ratio = 1 / 30  # DeepMind paper
+        random.seed(0)
 
-		states = np.reshape(states, (-1, self.board_size, self.board_size, input_planes))
-		policies = np.reshape(policies, (-1, self.board_size ** 2 + 1))	
-		values = np.reshape(values, (-1, 1))
-		
-		# Data splitting
-		print("Data splitting")
-		len_dataset = len(values)		
-		test_size = int(len_dataset * test_ratio)		
-		if test_size != 0:
-			b_split = int(len_dataset * test_ratio * k)
-			e_split = int((b_split + len_dataset * test_ratio) % len_dataset)
-			test_states, test_policies, test_values = states[b_split:e_split], policies[b_split:e_split], values[b_split:e_split]
-			train_states = np.concatenate([states[0:b_split], states[e_split:]])
-			train_policies = np.concatenate([policies[0:b_split], policies[e_split:]])
-			train_values = np.concatenate([values[0:b_split], values[e_split:]])
-			
-			validation_states, validation_policies, validation_values = test_states, test_policies, test_values
+        maxK = int(1 / test_ratio)
+        k = min(k, maxK - 1)
 
-		# Training
-		print("Training")
-		self.neural_network.save_model()
-		len_train = train_states.shape[0]
-		for i in range(1,epoch):
-			# Get batch
-			if(batch_size == len_train):
-				batch_states, batch_policies, batch_values = train_states, train_policies, train_values
-			else:
-				idx = []
-				while len(idx) < batch_size:
-					idx.append(np.random.randint(low=0, high=len_train))
-				batch_states, batch_policies, batch_values = train_states[idx], train_policies[idx], train_values[idx]
+        # Load dataset
+        print("Data loading")
+        npzfile = np.load(dataset)
+        t_states = npzfile['states']
+        t_policies = npzfile['policies']
+        t_values = npzfile['values']
+        player_turn = npzfile['player_turn']
 
-			# Train model on this batch
-			loss, p_acc, v_err = self.neural_network.train(batch_states, batch_policies, batch_values, epoch)
-			
-			# Print results
-			if i % report_frequency == 0:
-				print("\nMinibatch {} : \nloss = {}".format(i, loss))
-				print("TRAINING  :\npolicy accuracy = {:.4f}\nvalue  error    = {:.4f}".format(p_acc, v_err))
-				
-			if i % validation_frequency == 0:
-				if test_size != 0:
-					val_p_acc, val_v_err, p_out, v_out = self.neural_network.feed_forward_accuracies(validation_states, validation_policies, validation_values, epoch)				
-					print("\nVALIDATION:\npolicy accuracy = {:.4f}\nvalue  error    = {:.4f}".format(val_p_acc, val_v_err))	
-				print()
-			
-			if i % save_frequency == 0:
-				self.neural_network.save_model(False)
+        # TODO - remove the next lines to work on the full dataset
+        # pre-shuffle
+        print("(pre-shuffle)")
+        temp = list(zip(t_states, t_policies, t_values))
+        shuffle(temp)
+        t_states, t_policies, t_values = zip(*temp)
+        t_states, t_policies, t_values = np.array(t_states), np.array(t_policies), np.array(t_values)
 
-		print("Optimization Finished!")
-		test_p_acc, test_v_err, _, _ = self.neural_network.feed_forward_accuracies(test_states, test_policies, test_values, 0)
-		print("TEST      :\npolicy accuracy = {:.4f}\nvalue  error    = {:.4f}".format(test_p_acc, test_v_err))
+        t_states = t_states[:data_size]
+        t_policies = t_policies[:data_size]
+        t_values = t_values[:data_size]
+        player_turn = player_turn[:data_size]
 
+        # Shape states to neural network input shape
+        print("Data shaping")
+        tt_states = []
+        tt_policies = []
+        for i in range(len(t_states)):
+            player_feature_plane = np.full((self.board_size, self.board_size), player_turn[i])
+            # t_state = ops.goban_to_nn_state(t_states[i], self.board_size)
+            t_state = t_states[i]
+            player_feature_plane = ops.goban_to_nn_state(player_feature_plane, self.board_size)
+            tt_states.append(np.concatenate([t_state, player_feature_plane], axis=3))
+            tt_policies.append(np.reshape(t_policies[i], (1, self.board_size ** 2 + 1)))
+        t_states = np.array(tt_states)
+        t_policies = np.array(tt_policies)
+
+        input_planes = t_states.shape[-1]
+
+        # Data augmentation
+        print("Data augmentation")
+        states, policies, values = [], [], []
+        for i in range(len(t_states)):
+            state, policy, value = t_states[i], t_policies[i], t_values[i]
+            new_states, new_policies = ops.data_augmentation(state, policy, self.board_size)
+            for j in range(len(new_states)):
+                states.append(new_states[j])
+                policies.append(new_policies[j])
+                values.append(value)
+
+        # Shuffle
+        print("Data shuffling")
+        temp = list(zip(states, policies, values))
+        shuffle(temp)
+        states, policies, values = zip(*temp)
+        states, policies, values = np.array(states), np.array(policies), np.array(values)
+
+        states = np.reshape(states, (-1, self.board_size, self.board_size, input_planes))
+        policies = np.reshape(policies, (-1, self.board_size ** 2 + 1))
+        values = np.reshape(values, (-1, 1))
+
+        # Data splitting
+        print("Data splitting")
+        len_dataset = len(values)
+        test_size = int(len_dataset * test_ratio)
+        if test_size != 0:
+            b_split = int(len_dataset * test_ratio * k)
+            e_split = int((b_split + len_dataset * test_ratio) % len_dataset)
+            test_states, test_policies, test_values = states[b_split:e_split], policies[b_split:e_split], values[
+                                                                                                          b_split:e_split]
+            train_states = np.concatenate([states[0:b_split], states[e_split:]])
+            train_policies = np.concatenate([policies[0:b_split], policies[e_split:]])
+            train_values = np.concatenate([values[0:b_split], values[e_split:]])
+
+            validation_states, validation_policies, validation_values = test_states, test_policies, test_values
+
+        # Training
+        print("Training")
+        self.neural_network.save_model()
+        len_train = train_states.shape[0]
+        for i in range(1, epoch):
+            # Get batch
+            if batch_size == len_train:
+                batch_states, batch_policies, batch_values = train_states, train_policies, train_values
+            else:
+                idx = []
+                while len(idx) < batch_size:
+                    idx.append(np.random.randint(low=0, high=len_train))
+                batch_states, batch_policies, batch_values = train_states[idx], train_policies[idx], train_values[idx]
+
+            # Train model on this batch
+            loss, p_acc, v_err = self.neural_network.train(batch_states, batch_policies, batch_values, epoch)
+
+            # Print results
+            if i % report_frequency == 0:
+                print("\nMinibatch {} : \nloss = {}".format(i, loss))
+                print("TRAINING  :\npolicy accuracy = {:.4f}\nvalue  error    = {:.4f}".format(p_acc, v_err))
+
+            if i % validation_frequency == 0:
+                if test_size != 0:
+                    val_p_acc, val_v_err, p_out, v_out = self.neural_network.feed_forward_accuracies(validation_states,
+                                                                                                     validation_policies,
+                                                                                                     validation_values,
+                                                                                                     epoch)
+                    print("\nVALIDATION:\npolicy accuracy = {:.4f}\nvalue  error    = {:.4f}".format(val_p_acc,
+                                                                                                     val_v_err))
+                print()
+
+            if i % save_frequency == 0:
+                self.neural_network.save_model(False)
+
+        print("Optimization Finished!")
+        test_p_acc, test_v_err, _, _ = self.neural_network.feed_forward_accuracies(test_states, test_policies,
+                                                                                   test_values, 0)
+        print("TEST      :\npolicy accuracy = {:.4f}\nvalue  error    = {:.4f}".format(test_p_acc, test_v_err))
 
 
 if __name__ == '__main__':
-	
-	if len(sys.argv) != 1:
-		# Supervised training
-		board_size = 19
-		goAgent = GoNNAgent(board_size)	
-		goAgent.supervised_training(sys.argv[1])
-	else:
-		# Reinforcement training
-		board_size = 13
-		goAgent = GoNNAgent(board_size)
 
-		turn_max = int(board_size ** 2 * 2.)
+    if len(sys.argv) != 1:
+        # Supervised training
+        board_size = 19
+        goAgent = GoNNAgent(board_size)
+        goAgent.supervised_training(sys.argv[1])
+    else:
+        # Reinforcement training
+        board_size = 13
+        goAgent = GoNNAgent(board_size)
 
-		for i in range(1000):
+        turn_max = int(board_size ** 2 * 2.)
 
-			g = IGame(board_size)
-			g.display()
+        for i in range(1000):
 
-			total_turn = 0
-			while not g.over():
-				print("game {} - total_turn = {}".format(i, total_turn))
-				#goban = g.goban()
-				goban = g.raw_goban_split()
-				move = goAgent.get_move(goban, total_turn % 2, g.legals())
-				t_move = ops.move_scalar_to_tuple(move, board_size)
-				if t_move in g.legals():
-					print(t_move)			
-					g.play(t_move)
-				else:
-					print("(skip_move)")
-					g.play(None)
-				g.display()
-				total_turn += 1	
-				if total_turn > turn_max:
-					break
+            g = IGame(board_size)
+            g.display()
 
-			if total_turn > turn_max:	
-				winner = 2 # draw
-				print("(Draw due to maximum moves)")
-			else:
-				score = g.outcome()
-				print(score)
-				winner = 2 if score[0] == score[1] else 0 if score[0] > score[1] else 1			
-			goAgent.end_game(winner)
+            total_turn = 0
+            while not g.over():
+                print("game {} - total_turn = {}".format(i, total_turn))
+                # goban = g.goban()
+                goban = g.raw_goban_split()
+                move = goAgent.get_move(goban, total_turn % 2, g.legals())
+                t_move = ops.move_scalar_to_tuple(move, board_size)
+                if t_move in g.legals():
+                    print(t_move)
+                    g.play(t_move)
+                else:
+                    print("(skip_move)")
+                    g.play(None)
+                g.display()
+                total_turn += 1
+                if total_turn > turn_max:
+                    break
 
+            if total_turn > turn_max:
+                winner = 2  # draw
+                print("(Draw due to maximum moves)")
+            else:
+                score = g.outcome()
+                print(score)
+                winner = 2 if score[0] == score[1] else 0 if score[0] > score[1] else 1
+            goAgent.end_game(winner)
 
 """l = g.legals()
 print(l)
